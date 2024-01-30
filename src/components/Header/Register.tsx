0f40cb9f8bab478ff8f9ce03ebccb305a2bd3c2c
AnastasiaNag <nasivannag@gmail.com>
2024-01-30 19:38:28 +0300

    header

--- src/components/Header/Register.tsx
+++ src/components/Header/Register.tsx
@@ -1,25 +1,24 @@
-import { TextField, Button } from '@mui/material';
+import { TextField, Button } from "@mui/material";
 
-const Register = ({ changeMode }) => {
-    return (
-        <>
-            <h2 className="modal__title">Sign Up</h2>
-            <div className="modal__inputs">
-                <TextField label="Username" variant="filled" fullWidth />
-                <TextField label="Password" variant="filled" fullWidth />
-            </div>
-            <div className="modal__link-wrap">
-                Already a redditor?
-                <span onClick={changeMode} className="modal__link">
-                    Log In
-                </span>
-            </div>
-
-            <Button className="modal__button" variant="contained" fullWidth>
-                Continue
-            </Button>
-        </>
-    );
-};
-
+const Register = ({changeMode}) => {
+    return ( <>
+        <h2 className="modal__title">Sing up</h2>
+        
+        <div className="modal__inputs">
+            <TextField label="Email" variant="filled" fullWidth />
+        </div>
+        
+        <div className="modal__link-wrap">
+            Already a redditor?{' '}
+            <span onClick={changeMode} className="modal__link">
+                Log in
+            </span>
+        </div>
+        
+        <Button className="modal__button" variant="contained" fullWidth>
+            Log In
+        </Button>
+        </> );
+}
 export default Register;
+
