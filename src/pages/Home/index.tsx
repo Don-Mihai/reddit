9659dc79b6c8af4f8aedb909cfa175f2757e4701
Alexandr <a.s.navtsenya@gmail.com>
2024-01-30 19:41:58 +0300

    aside

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,54 @@ const Home = () => {
             <main className="main">
                 <nav className="nav"></nav>
                 <div className="content"></div>
-                <aside className="aside"></aside>
+                
+                <aside className="aside">
+                    <div className="aside__popular">POPULAR COMMUNITIES</div>
+                    <div className="aside__popular-list">
+                        <div className="popular-list__container">
+                            <a className="logo" href=''>
+                                <img className='logo__img' src='../Popular/Images/KZESzgF91cP3KEAR29JhCFmX0zxsPgY1sYhv7XCtiW0.jpg'></img>
+                                <div className='logo__name'>
+                                    <div className="logo__name__text">r/explainlikeimfive</div>
+                                    <div className="logo__name__members">22,736,231 members</div>
+                                </div>
+                            </a>
+
+                            <a className="logo" href=''>
+                                <img className='logo__img' src='../Popular/Images/KZESzgF91cP3KEAR29JhCFmX0zxsPgY1sYhv7XCtiW0.jpg'></img>
+                                <div className='logo__name'>
+                                    <div className="logo__name__text">r/explainlikeimfive</div>
+                                    <div className="logo__name__members">22,736,231 members</div>
+                                </div>
+                            </a>
+
+                            <a className="logo" href=''>
+                                <img className='logo__img' src='../Popular/Images/KZESzgF91cP3KEAR29JhCFmX0zxsPgY1sYhv7XCtiW0.jpg'></img>
+                                <div className='logo__name'>
+                                    <div className="logo__name__text">r/explainlikeimfive</div>
+                                    <div className="logo__name__members">22,736,231 members</div>
+                                </div>
+                            </a>
+
+                            <a className="logo" href=''>
+                                <img className='logo__img' src='../Popular/Images/KZESzgF91cP3KEAR29JhCFmX0zxsPgY1sYhv7XCtiW0.jpg'></img>
+                                <div className='logo__name'>
+                                    <div className="logo__name__text">r/explainlikeimfive</div>
+                                    <div className="logo__name__members">22,736,231 members</div>
+                                </div>
+                            </a>
+
+                            <a className="logo" href=''>
+                                <img className='logo__img' src='../Popular/Images/KZESzgF91cP3KEAR29JhCFmX0zxsPgY1sYhv7XCtiW0.jpg'></img>
+                                <div className='logo__name'>
+                                    <div className="logo__name__text">r/explainlikeimfive</div>
+                                    <div className="logo__name__members">22,736,231 members</div>
+                                </div>
+                            </a>
+
+                        </div>
+                    </div>
+                </aside>
             </main>
         </div>
     );
