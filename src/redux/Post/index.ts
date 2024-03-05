e94df6b5e42f27fd694a9612064d583b1b966e0f
mihai807 <mihai807@mail.ru>
2024-03-05 21:10:40 +0300

    lint

--- src/redux/Post/index.ts
+++ src/redux/Post/index.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { PostState, IPost, PCreatePost } from "./types";
 import axios from 'axios';
 
