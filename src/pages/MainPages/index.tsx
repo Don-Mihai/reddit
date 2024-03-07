0d19087d42969fa6af1ed1a995619a8b46794707
Sveta Konstantinova <svetikons@mail.ru>
2024-03-07 21:36:48 +0300

    fix post

--- src/pages/MainPages/index.tsx
+++ src/pages/MainPages/index.tsx
@@ -1,22 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header';
-import Nav from '../../components/Nav/Nav';
+import Nav from '../../components/Nav';
 import Popular from '../Home/Popular';
 
 const MainPages = () => {
-    return (
-        <div>
-            <Header />
+  return (
+    <div>
+      <Header />
 
-            <main className="main">
-                <Nav />
-                <div className="content">
-                    <Outlet />
-                </div>
-                <Popular />
-            </main>
+      <main className="main">
+        <Nav />
+        <div className="content">
+          <Outlet />
         </div>
-    );
+        <Popular />
+      </main>
+    </div>
+  );
 };
 
 export default MainPages;
