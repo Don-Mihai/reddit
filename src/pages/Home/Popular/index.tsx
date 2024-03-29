1a239e549fc68cc79e7266fae6b1aae5a8b23c7e
Sveta Konstantinova <svetikons@mail.ru>
2024-03-29 20:30:44 +0300

    fix aside

--- src/pages/Home/Popular/index.tsx
+++ src/pages/Home/Popular/index.tsx
@@ -1,53 +1,52 @@
 import { Avatar } from '@mui/material';
 import { memo } from 'react';
+import './Popular.scss';
 
 const Popular = memo(() => {
   return (
     <aside className="aside">
       <div className="aside__popular">POPULAR COMMUNITIES</div>
       <div className="aside__popular-list">
-        <div className="popular-list__container">
-          <a className="logo" target="blank" href="/">
-            <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
-            <div className="logo__name">
-              <div className="logo__name__text">r/explainlikeimfive</div>
-              <div className="logo__name__members">22,736,231 members</div>
-            </div>
-          </a>
+        <a className="logo" target="blank" href="/">
+          <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
+          <div className="logo__name">
+            <div className="logo__name__text">r/explainlikeimfive</div>
+            <div className="logo__name__members">22,736,231 members</div>
+          </div>
+        </a>
 
-          <a className="logo" href="/">
-            <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
-            <div className="logo__name">
-              <div className="logo__name__text">r/explainlikeimfive</div>
-              <div className="logo__name__members">22,736,231 members</div>
-            </div>
-          </a>
+        <a className="logo" href="/">
+          <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
+          <div className="logo__name">
+            <div className="logo__name__text">r/explainlikeimfive</div>
+            <div className="logo__name__members">22,736,231 members</div>
+          </div>
+        </a>
 
-          <a className="logo" href="/">
-            <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
-            <div className="logo__name">
-              <div className="logo__name__text">r/explainlikeimfive</div>
-              <div className="logo__name__members">22,736,231 members</div>
-            </div>
-          </a>
+        <a className="logo" href="/">
+          <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
+          <div className="logo__name">
+            <div className="logo__name__text">r/explainlikeimfive</div>
+            <div className="logo__name__members">22,736,231 members</div>
+          </div>
+        </a>
 
-          <a className="logo" href="/">
-            <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
+        <a className="logo" href="/">
+          <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
 
-            <div className="logo__name">
-              <div className="logo__name__text">r/explainlikeimfive</div>
-              <div className="logo__name__members">22,736,231 members</div>
-            </div>
-          </a>
+          <div className="logo__name">
+            <div className="logo__name__text">r/explainlikeimfive</div>
+            <div className="logo__name__members">22,736,231 members</div>
+          </div>
+        </a>
 
-          <a className="logo" href="/">
-            <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
-            <div className="logo__name">
-              <div className="logo__name__text">r/explainlikeimfive</div>
-              <div className="logo__name__members">22,736,231 members</div>
-            </div>
-          </a>
-        </div>
+        <a className="logo" href="/">
+          <Avatar src="https://avatars.akamai.steamstatic.com/bf9c5efeb726c14f07e66c408424067149a97724.jpg" alt="" />
+          <div className="logo__name">
+            <div className="logo__name__text">r/explainlikeimfive</div>
+            <div className="logo__name__members">22,736,231 members</div>
+          </div>
+        </a>
       </div>
     </aside>
   );
